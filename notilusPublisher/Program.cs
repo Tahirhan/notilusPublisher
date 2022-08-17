bae731159a0ac1e60f41674b6c3b868ad91cfbdd
Tahirhan <tahirhanyildizoglu3@gmail.com>
2022-08-17 09:59:01 +0300

    total commit

--- notilusPublisher/Program.cs
+++ notilusPublisher/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace notilusPublisher
 {
+    enum Result
+    {
+        SUCCESS,
+        FAILURE
+    };
+
     class Program
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> pathPairs = new Dictionary<string, string>();
+            pathPairs.Add(@"C:\Users\Tahirhan\source\repos\notilussteeler\NotilusSteeler", @"\\NOTILUSNAS\Server\15- Notilus Software\NotilusSteeler\Versions");
+            pathPairs.Add(@"C:\Users\Tahirhan\source\repos\NotilusDrafting\NotilusDrafting", @"\\NOTILUSNAS\Server\15- Notilus Software\Notilus Drafting\Versions");
+            pathPairs.Add(@"C:\Users\Tahirhan\source\repos\NotilusPiping\NotilusPiping", @"\\NOTILUSNAS\Server\15- Notilus Software\NotilusPiping\Versions");
+            pathPairs.Add(@"C:\Users\Tahirhan\source\repos\NotilusTools\NotilusTools", @"\\NOTILUSNAS\Server\15- Notilus Software\NotilusTools\Versions");
+            pathPairs.Add(@"C:\Users\Tahirhan\source\repos\NotilusRulemaster\NotilusRulemaster", @"\\NOTILUSNAS\Server\15- Notilus Software\NotilusRulemaster\Versions");
+            pathPairs.Add(@"C:\Users\Tahirhan\source\repos\NotilusPID\NotilusPID", @"\\NOTILUSNAS\Server\15- Notilus Software\NotilusPID\Versions");
+            pathPairs.Add(@"C:\Users\Tahirhan\source\repos\NotilusOnBoard\NotilusOnBoard", @"\\NOTILUSNAS\Server\15- Notilus Software\NotilusOnBoard\Versions");
+            pathPairs.Add(@"C:\Users\Tahirhan\source\repos\Notilus_Clipper\Notilus_Clipper", @"\\NOTILUSNAS\Server\15- Notilus Software\Notilus_Clipper\Versions_");
+
+            csServerPublisher serverPublisher = new csServerPublisher(pathPairs);
+            serverPublisher.run();
         }
     }
 }
---
+++ notilusPublisher/csServerPublisher.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notilusPublisher
+{
+    class csServerPublisher
+    {
+        Dictionary<string, string> pluginBinAndExportPaths = new Dictionary<string, string>();
+
+        public csServerPublisher(Dictionary<string, string> _pluginBinAndExportPaths)
+        {
+            pluginBinAndExportPaths = _pluginBinAndExportPaths;
+        }
+
+        public Result run()
+        {
+            string dateFolderStr = getDateFolderString();
+
+            foreach (string key in this.pluginBinAndExportPaths.Keys)
+            {
+                try
+                {
+                    string pluginName = key.Split(@"\".ToArray()).Last();
+                    string pathToCopyBin = deleteAndCreateNewDateFolder(dateFolderStr, this.pluginBinAndExportPaths[key]);
+                    CopyBinToPath(key, pathToCopyBin);
+                    CreateRhiFile(pathToCopyBin, pluginName);
+                    Console.WriteLine($"[notilusPublisher] Published {pluginName}.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[notilusPublisher] Failed to publish {key.Split(@"\".ToArray()).Last()}.");
+                }
+            }
+
+            return Result.SUCCESS;
+        }
+
+        private void CreateRhiFile(string pathToCopyBin, string pluginName)
+        {
+            ZipFile.CreateFromDirectory(pathToCopyBin, pathToCopyBin.Replace(@"\bin", @"\"+pluginName+".rhi"));
+        }
+
+        private void CopyBinToPath(string binPath, string pathToCopyBin)
+        {
+            binPath = Path.GetFullPath(Path.Combine(binPath, "bin"));
+            string[] allDirectories = Directory.GetDirectories(binPath, "*", SearchOption.AllDirectories);
+            foreach (string dir in allDirectories)
+            {
+                string dirToCreate = dir.Replace(binPath, pathToCopyBin);
+                DirectoryInfo directoryInfo = Directory.CreateDirectory(dirToCreate);
+            }
+
+            var allFiles = Directory.GetFiles(binPath, "*.*", SearchOption.AllDirectories);
+            foreach (string newPath in allFiles)
+            {
+                File.Copy(newPath, newPath.Replace(binPath, pathToCopyBin), true);
+            }
+        }
+
+        private string deleteAndCreateNewDateFolder(string dateFolderStr, string exportPath)
+        {
+            string folderPath = Path.GetFullPath(Path.Combine(exportPath, dateFolderStr));
+            if (Directory.Exists(folderPath)) Directory.Delete(folderPath, true);
+            DirectoryInfo directoryInfo = Directory.CreateDirectory(folderPath);
+            folderPath = Path.GetFullPath(Path.Combine(folderPath, "bin"));
+            directoryInfo = Directory.CreateDirectory(folderPath);
+            return folderPath;
+        }
+
+        private string getDateFolderString()
+        {
+            return DateTime.Now.ToString("yyyy.MM.dd").Substring(2);
+        }
+    }
+}
